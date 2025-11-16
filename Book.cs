80eb3210f983092fa703704d15d20cd11205de54
Semen <hrams205@gmail.com>
2025-11-16 18:57:44 +0300

    ready, set, go: add Book.cs

---
+++ Book.cs
@@ -0,0 +1,76 @@
+namespace Bookstore;
+
+public class Book
+{
+  // private fields
+  private double? price;
+  private int? stock;
+
+  // access to them via properties
+  public string Isbn { get; private set; }
+
+  public string Title { get; private set; }
+
+  public string Author { get; private set; }
+
+  public double Price
+  {
+    get => price!.Value;
+    private set
+    {
+      price = value;
+      if (value < 0)
+      {
+        throw new ArgumentOutOfRangeException($"expected {nameof(price)} >= 0, but given: {price}");
+      }
+    }
+  }
+
+  public int Stock
+  {
+    get => stock!.Value;
+    private set
+    {
+      stock = value;
+      if (value < 0)
+      {
+        throw new ArgumentOutOfRangeException($"expected {nameof(stock)} >= 0, but given: {stock}");
+      }
+    }
+  }
+
+  public bool IsAvailable => Stock > 0;
+
+  public void Rent()
+  {
+    if (!IsAvailable)
+    {
+      throw new InvalidOperationException("rent a book, but this type of books are out");
+    }
+    Stock--;
+  }
+
+  public void Return()
+  {
+    Stock++;
+  }
+
+  public void Reprice(double newPrice)
+  {
+    Price = newPrice;
+  }
+
+  override public string ToString()
+  {
+    return $"{Author} - {Title} ({Isbn}) ${Price} [{Stock}]";
+  }
+
+  public Book(string isbn, string title, string author, double price, int stock)
+  {
+    Isbn = isbn;
+    Title = title;
+    Author = author;
+    Price = price;
+    Stock = stock;
+  }
+};
---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿using Bookstore;
+
+Book b = new("978-5-4461-0923-5", "CLR via C#", "Jeffrey Richter", 55.0, 3);
+
+Console.WriteLine($"{b}");
